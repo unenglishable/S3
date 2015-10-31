0d554144173254082cb72af69c43da378d52b34e
unenglishable <unenglishable@gmail.com>
2015-10-30 14:01:39 -1000

    remove aisis

--- aRBitrator.rb
+++ aRBitrator.rb
@@ -1,2 +1,2 @@
-team = ['james', 'julie', 'ed', 'anthony', 'ryan', 'bronson', 'aisis', 'joseph']
+team = ['james', 'julie', 'ed', 'anthony', 'ryan', 'bronson', 'joseph']
 p team.shuffle.insert(team.size/2, 'intermission')
